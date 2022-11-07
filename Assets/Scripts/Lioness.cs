8ffc91b5b834aaa5b0bdd2d373c480534d86fbd9
Eugene Lebedev <mr.legenius@gmail.com>
2022-11-07 08:11:48 +0300

    Add drinking behavior to lioness

--- Assets/Scripts/Lioness.cs
+++ Assets/Scripts/Lioness.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Lev.BehaviorTree;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,11 +7,32 @@ namespace Lev
 {
 public class Lioness : MonoBehaviour
 {
-    [SerializeField] private float _areaRadius;
-    [SerializeField] private float _sleepTime;
+    [Header("Thirst")]
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float _thirstLevel = 0.3f;
+    [SerializeField] private float _thirstGrowthRate = 0.01f;
+    
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float _thirstThreshold = 0.4f;
+    [SerializeField] private float _drinkRate = 0.01f;
+    [SerializeField] private GameObject _waterSource;
+    
+    [Header("Rest")]
+    [SerializeField] private float _restAreaRadius;
+    [SerializeField] private float _restTime;
+    
+    [Header("Dependencies")]
     [SerializeField] private NavMeshAgent _navMeshAgent;
     
     private RootBehaviorTreeNode _behaviorTree;
+
+    private bool _isDrinking;
+
+    private float ThirstLevel
+    {
+        get => _thirstLevel;
+        set => _thirstLevel = Mathf.Clamp01(value);
+    }
 
     private void Awake()
     {
@@ -24,17 +44,36 @@ public class Lioness : MonoBehaviour
         _behaviorTree.StartBehaviourTree();
     }
 
+    private void Update()
+    {
+        if (!_isDrinking)
+            ThirstLevel += _thirstGrowthRate;
+    }
+
     private void OnDestroy()
     {
         _behaviorTree.StopBehaviourTree();
     }
 
     private RootBehaviorTreeNode CreateBehaviorTree() =>
-        new(new SequenceBehaviorTreeNode(
+        new(new SelectorBehaviorTreeNode(
             new List<BehaviorTreeNode>
             {
-                new RoamingBehaviorTreeNode(transform.position, _areaRadius, _navMeshAgent),
-                new WaitBehaviorTreeNode(_sleepTime)
+                new SequenceBehaviorTreeNode( 
+                    new List<BehaviorTreeNode>
+                    {
+                        new MoveToTargetBehaviorTreeNode(_waterSource, _navMeshAgent),
+                        new WaterDrinkBehaviorTreeNode(() => ThirstLevel <= 0.1f, () => ThirstLevel -= _drinkRate)
+                            .OnEntered(() => _isDrinking = true)
+                            .OnExited(_ => _isDrinking = false),
+                    })
+                    .AddCondition(() => ThirstLevel > _thirstThreshold),
+                new SequenceBehaviorTreeNode(
+                    new List<BehaviorTreeNode>
+                    {
+                        new RoamingBehaviorTreeNode(transform.position, _restAreaRadius, _navMeshAgent),
+                        new WaitBehaviorTreeNode(_restTime),
+                    }),
             }));
 }
 }
---
+++ Assets/Scripts/WaterDrinkBehaviorTreeNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Lev.BehaviorTree;
+
+namespace Lev
+{
+public class WaterDrinkBehaviorTreeNode : BehaviorTreeNode
+{
+    private readonly Func<bool> _isFull;
+    private readonly Action _drink;
+
+    public WaterDrinkBehaviorTreeNode(Func<bool> isFull, Action drink)
+    {
+        _isFull = isFull;
+        _drink = drink;
+    }
+    protected override async UniTask<bool> ExecuteOverride(CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested && !_isFull())
+        {
+            _drink();
+            await UniTask.Yield();
+        }
+
+        return !ct.IsCancellationRequested;
+    }
+}
+}
