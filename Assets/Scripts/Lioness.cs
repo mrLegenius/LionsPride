05029677f28294029ad7c4f354d0f607afbafe9e
Eugene Lebedev <mr.legenius@gmail.com>
2022-11-07 06:47:43 +0300

    Implement random roaming lioness behavior

---
+++ Assets/Scripts/Lioness.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Lev.BehaviorTree;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Lev
+{
+public class Lioness : MonoBehaviour
+{
+    [SerializeField] private float _areaRadius;
+    [SerializeField] private float _sleepTime;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+    
+    private RootBehaviorTreeNode _behaviorTree;
+
+    private void Awake()
+    {
+        _behaviorTree = CreateBehaviorTree();
+    }
+
+    private void Start()
+    {
+        _behaviorTree.StartBehaviourTree();
+    }
+
+    private void OnDestroy()
+    {
+        _behaviorTree.StopBehaviourTree();
+    }
+
+    private RootBehaviorTreeNode CreateBehaviorTree() =>
+        new(new SequenceBehaviorTreeNode(
+            new List<BehaviorTreeNode>
+            {
+                new RoamingBehaviorTreeNode(transform.position, _areaRadius, _navMeshAgent),
+                new WaitBehaviorTreeNode(_sleepTime)
+            }));
+}
+}
---
+++ Assets/Scripts/RoamingBehavior.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Lev.BehaviorTree;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamingBehaviorTreeNode : BehaviorTreeNode
+{
+    private readonly float _areaRadius;
+    private readonly Vector3 _areaCenter;
+    private readonly NavMeshAgent _navMeshAgent;
+
+    public RoamingBehaviorTreeNode(Vector3 areaCenter, float areaRadius, NavMeshAgent navMeshAgent)
+    {
+        _areaRadius = areaRadius;
+        _areaCenter = areaCenter;
+        _navMeshAgent = navMeshAgent;
+    }
+
+    public override async UniTask<bool> Execute(CancellationToken ct)
+    {
+        _navMeshAgent.SetDestination(GetRandomLocation());
+        await UniTask.WaitUntil(IsDestinationReached, cancellationToken: ct);
+        return !ct.IsCancellationRequested;
+    }
+
+    private bool IsDestinationReached() => 
+        _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance;
+
+    private Vector3 GetRandomLocation()
+    {
+        var randomPoint = Random.insideUnitCircle * _areaRadius;
+        return _areaCenter + new Vector3(randomPoint.x, 0.0f, randomPoint.y);
+    }
+}
